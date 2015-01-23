49a3f4d2eedc3739f4a099bf8807d305cf12f632
Chunyang Xu <xuchunyang56@gmail.com>
2015-01-23 16:28:38 +0800

    Misc update

--- init.el
+++ init.el
@@ -3,6 +3,14 @@
 (and (fboundp 'menu-bar-mode) (not (eq system-type 'darwin))
      (menu-bar-mode -1))
 
+;; Use org-mode from git repo
+(add-to-list 'load-path "~/repos/org-mode/lisp")
+(add-to-list 'load-path "~/repos/org-mode/contrib/lisp")
+
+(add-to-list 'load-path "/usr/local/Cellar/cask/0.7.2")
+(add-to-list 'load-path "/usr/local/share/emacs/site-lisp/mu4e/")
+
 (require 'org)
 (require 'ob-tangle)
+
 (org-babel-load-file (expand-file-name "~/.emacs.d/emacs-config.org"))
