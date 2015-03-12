8387a33e11f5f4ffd1b765f7cd0cc37a41ab1e3e
Chunyang Xu <xuchunyang56@gmail.com>
2015-03-12 12:33:45 +0800

    github-notify: add basic implementation

--- personal/chunyang-github-notify.el
+++ personal/chunyang-github-notify.el
@@ -8,15 +8,36 @@
 ;;; Commentary:
 
 ;; TODO:
-;; - [ ] Fetch my Github notification
+;; - [x] Fetch my Github notification
 ;; - [ ] Shwo result (if not zero) in modeline
 ;;       (using Unicode number, or even Github icon?
-;; - [ ] Set up at timer
+;; - [ ] Set up at timer (must to be a minor mode? there are must some other way
+;;       to do this)
 
 ;;; Code:
 
-(defvar chunyang-github-refetch-interval 15
-  "Refetch period (min.")
+(defvar chunyang-github-notify-token nil
+  "Notification scope must be enabled.")
+
+(defconst chunyang-github-notify-api
+  "https://api.github.com/notifications")
+
+(defun chunyang-github-notify--get-conut ()
+  "Return nil if failed."
+  (let ((rtv (shell-command-to-string   ; FIXME: better need error check
+              (format
+               "curl --silent -H 'Authorization: token %s' %s | jq '. | length'"
+               chunyang-github-notify-token
+               chunyang-github-notify-api))))
+    (when (<= (length rtv) 3)            ; indicate 99 as the max conut
+      (string-to-number rtv))))
+
+(defun chunyang-github-notify ()
+  "Fetch notifications conut and update modeline."
+  (let ((conut (chunyang-github-notify--get-conut)))
+    (if (not (null conut))
+        (setq mode-line-format (format "Github [%d]" conut))
+      (user-error "[Github] Get notifications failed"))))
 
 (provide 'chunyang-github-notify)
 
