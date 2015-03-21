c008f33f99dc1f5d032ec2ea19a7466be8ac526b
Chunyang Xu <xuchunyang56@gmail.com>
2015-03-21 23:46:01 +0800

    ADd chunyang-format-time, new function

--- personal/chunyang-simple.el
+++ personal/chunyang-simple.el
@@ -11,5 +11,13 @@
   (select-window (split-window-right))
   (switch-to-buffer (other-buffer)))
 
+
+(defun chunyang-format-time (time)
+  "Convert 'October 20, 2014' to '2014-10'."
+  (format-time-string
+   "%Y-%m"
+   (apply 'encode-time (parse-time-string
+                        (concat time ", 12:12:12")))))
+
 (provide 'chunyang-simple)
 ;;; chunyang-simple.el ends here
